8f670bd001439c2a55b1455c9b29ddcc9bad2c22
tarious <f3245678@gmail.com>
2022-12-10 03:24:24 +0800

    first commit

---
+++ bot.config.js
@@ -0,0 +1,9 @@
+const linebot = require('linebot');
+
+var bot = linebot({
+    channelId: '1657724200',
+    channelSecret: 'ae1a089aea2db0b525c2fbe7fe7e503d',
+    channelAccessToken: 't9FdPyBR2UWx+HUE+hSbm8d9iwftJzKTxsJMRHlxQ3OZohR78o4I+EP5bCOWN4UYZhyiJF0d1+6vQzMY4U3i9BWSTLf5tGDuLNwAb5Y+txlMUgJXSHV8jSlEyBHu8Tx8kitxOyDdxdqD1dALn5rRpgdB04t89/1O/w1cDnyilFU=',
+})
+
+module.exports = bot
---
+++ index.js
@@ -0,0 +1,24 @@
+const linebot = require('linebot');
+const express = require('express');
+const mongoose = require('mongoose');
+const linebotParser = require('./bot.event')
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb+srv://admin:<password>@cluster0.eals7nz.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const app = express();
+// Handle static files
+app.use('/images',express.static(__dirname+'/images'));
+
+// Handle webhook
+app.post('/', linebotParser);
+
+// Routes
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, console.log(`Ninja Hattori is running on port ${PORT}`))
